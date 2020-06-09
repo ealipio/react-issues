b360a6ece9e9695aece9cac613020f3bf937db08
Eisson Alipio <eisson.alipio@gmail.com>
2020-06-09 09:11:54 -0500

    fix: no search empty query

--- src/sagas/issues/list.js
+++ src/sagas/issues/list.js
@@ -5,14 +5,16 @@ import { actionTypes as issuesActions } from '../../features/issues';
 
 export default function* tryUsersList({ query }) {
   try {
-    const URL = `https://api.github.com/search/issues?q=${query}+repo:facebook/react`;
-    const response = yield call(fetch, URL);
-    const data = yield call([response, 'json']);
+    if (query) {
+      const URL = `https://api.github.com/search/issues?q=${query}+repo:facebook/react`;
+      const response = yield call(fetch, URL);
+      const data = yield call([response, 'json']);
 
-    yield put({
-      type: issuesActions.SEARCH_SUCCESS,
-      data: _get(data, 'items', []),
-    });
+      yield put({
+        type: issuesActions.SEARCH_SUCCESS,
+        data: _get(data, 'items', []),
+      });
+    }
   } catch (e) {
     yield put({ type: issuesActions.SEARCH_FAILED });
   }
